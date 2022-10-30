7c822bdcc5401c86a5544c8765d964c5b333f5ea
torlundgren <70590253+torlundgren@users.noreply.github.com>
2022-10-30 08:46:58 -0700

    Initial Commit

---
+++ Hoodoo/HoodooApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoodooApp.swift
+//  Hoodoo
+//
+//  Created by Tor Lundgren on 10/30/22.
+//
+
+import SwiftUI
+
+@main
+struct HoodooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
